b9991a6e3144eb71cbdf4777cadfc5642a32ed3f
Stoil Tomov <tankeras@abv.bg>
2018-03-12 18:42:56 +0200

    Added Problem2.cs

---
+++ intro-and-basic-syntax/exercise/Problem2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var c = a * b;
+            Console.WriteLine("{0:F2}", c);
+
+
+
+
+        }
+    }
+}
