31b263d5544dedc745fc7ad5503010df5e960c66
Z3RKZ <coldstrike9@abv.bg>
2018-03-13 11:07:37 -0800

    added com

--- intro-and-basic-syntax/exercise/Problem2.cs
+++ intro-and-basic-syntax/exercise/Problem2.cs
@@ -10,10 +10,12 @@ namespace ConsoleApp18
         static void Main(string[] args)
         {
 
-
+// we put vars
             var a = double.Parse(Console.ReadLine());
             var b = double.Parse(Console.ReadLine());
             var c = a * b;
+
+//We print numbers
             Console.WriteLine("{0:F2}", c);
 
 
