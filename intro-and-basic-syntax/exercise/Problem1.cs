c865b1ff1aff8473feaa2c5e8e7d72ae9a3ef4f9
Z3RKZ <coldstrike9@abv.bg>
2018-03-13 11:07:14 -0800

    added com

--- intro-and-basic-syntax/exercise/Problem1.cs
+++ intro-and-basic-syntax/exercise/Problem1.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+
+//We put some numbs..
         int number1 = int.Parse(Console.ReadLine());
         int number2 = int.Parse(Console.ReadLine());
         int number3 = int.Parse(Console.ReadLine());
         int number4 = int.Parse(Console.ReadLine());
+
+//We print some things..
         Console.WriteLine("{0:d4} {0:d4} {0:d4} {0:d4}",number1,number2,number3,number4);
     }
 }
