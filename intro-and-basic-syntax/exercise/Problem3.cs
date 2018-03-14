233b6a8b9978be9aad106d108e81b9d4795f08c2
ScottMan <scottedomwonyi@Gmail.com>
2018-03-14 15:12:33 +0200

    This is Problem3.cs

--- intro-and-basic-syntax/exercise/Problem3.cs
+++ intro-and-basic-syntax/exercise/Problem3.cs
@@ -1,47 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-using System.Numerics;
- 
+
+ //How about this 
 namespace ConsoleApp22
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            BigInteger hightesSnowballValue= 0;
-
-            int MAXSnowballSnow = 0;
-            int MAXsnowballTime = 0;
-            int MAXsnowballQuality = 0;
-
-            for (int i = 0; i < N; i++)
-            {
-                int SnowballSnow = int.Parse(Console.ReadLine());
-                int snowballTime = int.Parse(Console.ReadLine());
-                int snowballQuality = int.Parse(Console.ReadLine());
-
-                BigInteger currentValue = BigInteger.Pow((SnowballSnow / snowballTime),snowballQuality );
-
-             
-                if (currentValue > hightesSnowballValue)
-                {
-                    hightesSnowballValue = currentValue;
-                    MAXSnowballSnow = SnowballSnow;
-                    MAXsnowballTime = snowballTime;
-                    MAXsnowballQuality = snowballQuality;
-                }
-
-              
-
-            }
-
-
-            Console.WriteLine($"{MAXSnowballSnow} : {MAXsnowballTime} = {hightesSnowballValue} ({MAXsnowballQuality})");
+            var mile = double.Parse(Console.ReadLine());
+            var kilometers = mile * 1.60934;
+            Console.WriteLine("{0:f2}", kilometers);
 
 
 
